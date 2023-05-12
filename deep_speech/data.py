9cdd58b3f43aeab73a6620ff689844cb12e9d12f
Akash Sonowal <work.akashsonowal@gmail.com>
2023-05-12 07:09:45 +0530

    Update data.py

--- deep_speech/data.py
+++ deep_speech/data.py
@@ -1,10 +1,15 @@
 import json
 
-char_map_str = json.load("char_map.json")
-
 class TextTransform:
   """Maps characters to integers and vice versa"""
   def __init__(self):
-    self.char_map_str = char_map_string
+    self.char_map_str = json.load(open("char_map.json", "r"))
+    self.char_map = {}
+    self.index_map = {}
+    for line in char_map_str.strip().split("\n"):
+      ch, index = line.split()
+      self.char_map[ch] = int(index)
+      self.index_map[int(index)] = ch
+    self.index_map[1] = ' '
     
   
