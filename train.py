1ac1549e0f37b98f28ba1af6e6d46ca2e57c1746
Akash Sonowal <work.akashsonowal@gmail.com>
2023-05-12 06:44:48 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,3 +1,13 @@
+import torchaudio
+
+train_dataset = torchaudio.datasets.LIBRISPEECH("./", url="train-clean-100", download=True)
+test_dataset = torchaudio.datasets.LIBRISPEECH("./", url="test-clean", download=True)
+
+torchaudio.transforms.FrequenceMasking()
+torchaudio.transforms.TimeMasking()
+
+
+
 def main():
   iter_meter = IterMeter()
   for epoch in range(1, epochs + 1):
