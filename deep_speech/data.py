7109464b480c0019b29c7c6681ee1c781b300309
akashsonowal <akashsonowal.cheme@gmail.com>
2023-05-14 08:57:22 +0530

    add update

--- deep_speech/data.py
+++ deep_speech/data.py
@@ -1,31 +1,33 @@
 import json
 
+
 class TextTransform:
-  """Maps characters to integers and vice versa"""
-  def __init__(self):
-    self.char_map_str = json.load(open("char_map.json", "r"))
-    self.char_map = {}
-    self.index_map = {}
-    for line in char_map_str.strip().split("\n"):
-      ch, index = line.split()
-      self.char_map[ch] = int(index)
-      self.index_map[int(index)] = ch
-    self.index_map[1] = ' '
-   
-  def text_to_int(self, text):
-    """Use a character map and convert text to an integer sequence"""
-    int_sequence = []
-    for c in text:
-      if c == ' ':
-        ch = self.char_map['']
-      else:
-        ch = self.char_map[c]
-      int_sequence.append(ch)
-    return int_sequence
-  
-  def int_to_text(self, labels):
-    """Use a character map and convert int labels to an text sequence"""
-    string = []
-    for i in labels:
-      string.append(self.index_map[i])
-    return ''.join(string).replace('', ' ')
+    """Maps characters to integers and vice versa"""
+
+    def __init__(self):
+        self.char_map_str = json.load(open("char_map.json", "r"))
+        self.char_map = {}
+        self.index_map = {}
+        for line in char_map_str.strip().split("\n"):
+            ch, index = line.split()
+            self.char_map[ch] = int(index)
+            self.index_map[int(index)] = ch
+        self.index_map[1] = " "
+
+    def text_to_int(self, text):
+        """Use a character map and convert text to an integer sequence"""
+        int_sequence = []
+        for c in text:
+            if c == " ":
+                ch = self.char_map[""]
+            else:
+                ch = self.char_map[c]
+            int_sequence.append(ch)
+        return int_sequence
+
+    def int_to_text(self, labels):
+        """Use a character map and convert int labels to an text sequence"""
+        string = []
+        for i in labels:
+            string.append(self.index_map[i])
+        return "".join(string).replace("", " ")
--- train.py
+++ train.py
@@ -1,20 +1,49 @@
 import torchaudio
 
-train_dataset = torchaudio.datasets.LIBRISPEECH("./", url="train-clean-100", download=True)
+train_dataset = torchaudio.datasets.LIBRISPEECH(
+    "./", url="train-clean-100", download=True
+)
 test_dataset = torchaudio.datasets.LIBRISPEECH("./", url="test-clean", download=True)
 
 train_audio_transforms = nn.Sequential(
-  torchaudio.transforms.MelSpectrogram(sample_rate=16000, n_mels=128),
-  torchaudio.transforms.FrequencyMasking(freq_mask_param=15),
-  torchaudio.transforms.TimeMasking(time_mask_param=35)
+    torchaudio.transforms.MelSpectrogram(sample_rate=16000, n_mels=128),
+    torchaudio.transforms.FrequencyMasking(freq_mask_param=15),
+    torchaudio.transforms.TimeMasking(time_mask_param=35),
 )
 valid_audio_transforms = torchaudio.transforms.MelSpectrogram()
 
-optimizer = optim.AdamW(model.parameters(), hparams['learning_rate'])
-scheduler = optim.lr_scheduler.OneCycleLR(optimizer, max_lr=hparams['learning_rate'], steps_per_epoch=int(len(train_loader)), epochs=hparams['epochs'], anneal_startegy='linear')
+optimizer = optim.AdamW(model.parameters(), hparams["learning_rate"])
+scheduler = optim.lr_scheduler.OneCycleLR(
+    optimizer,
+    max_lr=hparams["learning_rate"],
+    steps_per_epoch=int(len(train_loader)),
+    epochs=hparams["epochs"],
+    anneal_startegy="linear",
+)
+
 
 def main():
-  iter_meter = IterMeter()
-  for epoch in range(1, epochs + 1):
-    train(model, device, train_loader, criterion, optimizer, scheduler, epoch, iter_meter, experiment)
-    test(model, device, test_loader, criterion, optimizer, scheduler, epoch, iter_meter, experiment)
+    iter_meter = IterMeter()
+    for epoch in range(1, epochs + 1):
+        train(
+            model,
+            device,
+            train_loader,
+            criterion,
+            optimizer,
+            scheduler,
+            epoch,
+            iter_meter,
+            experiment,
+        )
+        test(
+            model,
+            device,
+            test_loader,
+            criterion,
+            optimizer,
+            scheduler,
+            epoch,
+            iter_meter,
+            experiment,
+        )
