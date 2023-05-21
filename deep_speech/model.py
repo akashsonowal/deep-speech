094d101799a0a7e5e977c848c26376e70da3aeb3
Akash Sonowal <work.akashsonowal@gmail.com>
2023-05-21 09:11:05 +0530

    update

---
+++ deep_speech/model.py
@@ -0,0 +1,28 @@
+import torch.nn as nn
+
+class CNNLayerNorm(nn.Module):
+    """Layer Normalization built for CNNs input"""
+    def __init__(self, n_feats):
+        pass
+
+    def forward(self, x):
+        pass
+
+class ResidualCNN(nn.Module):
+    def __init__(self):
+        pass
+    def forward(self, x):
+        pass
+    
+class BidirectionalGRU(nn.Module):
+    def __init__(self):
+        pass
+    
+    def forward(self, x):
+        pass 
+
+class SpeechRecognitionModel(nn.Module):
+    def __init__(self):
+        pass
+    def forward(self, x):
+        pass 
