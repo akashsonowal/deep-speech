88b815739af99755fbb448e1f8dcc61da7008e8c
akashsonowal <akashsonowal.cheme@gmail.com>
2023-05-21 12:28:05 +0530

    update

---
+++ util.py
@@ -0,0 +1,11 @@
+class IterMeter(object):
+    """Keeps track of total iterations"""
+
+    def __init__(self):
+        self.val = 0
+
+    def step(self):
+        self.val += 1
+
+    def get(self):
+        return self.val
