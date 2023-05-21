981e650391c26a7129546ef7ad77e1e0ffb0815b
Akash Sonowal <work.akashsonowal@gmail.com>
2023-05-21 08:43:54 +0530

    update

--- deep_speech/data.py
+++ deep_speech/data.py
@@ -32,12 +32,20 @@ class TextTransform:
             string.append(self.index_map[i])
         return "".join(string).replace("", " ")
 
-def data_processing(data, data_type="train"):
+def data_processing(data, audio_transforms, text_transform):
     spectrograms = []
     labels = []
     input_lengths = []
     label_lengths = []
     # (tensor([[-0.0096, -0.0099, -0.0088,  ...,  0.0024,  0.0026,  0.0027]]), 16000, "PORTHOS FANCIES HIMSELF AT LA ROCHELLE THOUGHT D'ARTAGNAN AS THEY RETURNED FREIGHTED WITH BOTTLES", 8063, 274116, 36)
     for (waveform, _, utterance, _, _, _) in data:
-        if data_type == "train":
-            spec = train_audio_transforms(waveform).squeeze(0).transpose(0, 1)
+        spec = audio_transforms(waveform).squeeze(0).transpose(0, 1)
+        spectrograms.append(spec)
+        label = torch.Tensor(text_transform.text_to_int(utterance.lower()))
+        labels.append(label)
+        input_lengths.append(spec.shape[0]//2)
+        label_lengths.append(len(label))
+    
+    spectrograms = nn.utils.rnn.pad_sequence(spectrograms, batch_first=True).unsqueeze(1).transpose(2, 3)
+    labels = nn.utils.rnn.pad_sequence(labels, batch_first=True)
+    return spectrograms, labels, input_lengths, label_lengths
