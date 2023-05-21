1b441460ceceba7816bd07ea3a267499592355b3
Akash Sonowal <work.akashsonowal@gmail.com>
2023-05-21 11:15:49 +0530

    update

--- deep_speech/model.py
+++ deep_speech/model.py
@@ -55,7 +55,15 @@ class BidirectionalGRU(nn.Module):
         return x 
 
 class SpeechRecognitionModel(nn.Module):
-    def __init__(self, rnn_dim, n_class, dropout=0.1):
+    def __init__(self, n_cnn_layers, n_rnn_layers, rnn_dim, n_class, dropout=0.1):
+        super(SpeechRecognitionModel, self).__init__()
+        n_feats = n_feats //2
+        self.cnn = nn.Conv2d(1, 32, 3, stride=stride, padding=3//2) # for extracting hierarcical features
+        self.rcnn_layers = nn.Sequential(*[ResidualCNN(32, 32, kernel=3, stride=1, dropout=dropout, n_feats=n_feats)
+                                           for _ in range(n_cnn_layers)])
+        self.fully_connected = nn.Linear(n_feats*32, rnn_dim)
+        self.birnn_layers = nn.Sequential(*[BidirectionalGRU(rnn_dim=rnn_dim if i == 0 else rnn_dim*2,
+                                                             hidden_size=rnn_dim, dropout=dropout, batch_first=i==0) for i in range(n_rnn_layers)])
         self.classifier = nn.Sequential(
             nn.Linear(rnn_dim*2, rnn_dim), #birnn returns rnn_dim*2
             nn.GELU(),
