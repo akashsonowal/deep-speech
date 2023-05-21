0d71071047eb0171f7176364e0a29a136cce5f4c
Akash Sonowal <work.akashsonowal@gmail.com>
2023-05-21 10:39:44 +0530

    update

--- deep_speech/model.py
+++ deep_speech/model.py
@@ -12,8 +12,18 @@ class CNNLayerNorm(nn.Module):
         return x.transpose(2, 3).contiguous() # x(batch, channel, feature, time)
 
 class ResidualCNN(nn.Module):
-    def __init__(self):
-        pass
+    """Residual CNN inspired by https://arxiv.org/pdf/1603.05027.pdf
+        except with layer norm instead of batch norm
+    """
+    def __init__(self, in_channels, out_channels, kernel , stride, dropout, n_feats):
+        super(ResidualCNN, self).__init__()
+        self.cnn1 = nn.Conv2d(in_channels, out_channels, kernel, stride, padding=kernel//2)
+        self.cnn2 = nn.Conv2d(in_channels, out_channels, kernel, stride, padding=kernel//2)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+        self.layer_norm1 = CNNLayerNorm(n_feats)
+        self.layer_norm2 = CNNLayerNorm(n_feats)
+
     def forward(self, x):
         pass
     
