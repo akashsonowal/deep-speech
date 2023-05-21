5c4b0038ac4de54161cb9bbdb13b0f3389f6e636
akashsonowal <akashsonowal.cheme@gmail.com>
2023-05-21 14:27:15 +0530

    update

--- util.py
+++ util.py
@@ -16,84 +16,10 @@ class IterMeter(object):
     def get(self):
         return self.val
 
+def avg_wer(wer_scores, combined_ref_len):
+    return float(sum(wer_scores)) / float(combined_ref_len)
 
-def train(
-    model,
-    device,
-    train_loader,
-    criterion,
-    optimizer,
-    scheduler,
-    epoch,
-    iter_meter,
-    experiment,
-):
-    model.train()
-    data_len = len(train_loader.dataset)
-    with experiment.train():
-        for batch_idx, _data in enumerate(train_loader):
-            spectrograms, labels, input_lengths, label_lengths = _data
-            spectrograms, labels = spectrograms.to(device), labels.to(device)
-            optimizer.zero_grad()
-            output = model(spectrograms)  # (batch, time, n_class)
-            output = F.log_softmax(output, dim=2)
-            output = output.transpose(0, 1)  # (time, batch, n_class)
-
-            loss = criterion(output, labels, input_lengths, label_lengths)
-            loss.backward()
-
-            experiment.log_metric("loss", loss.item(), step=iter_meter.get())
-            experiment.log_metric(
-                "learning_rate", scheduler.get_lr(), step=iter_meter.get()
-            )
-
-            optimizer.step()
-            scheduler.step()
-            iter_meter.step()
-            if batch_idx % 100 == 0 or batch_idx == data_len:
-                print(
-                    "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
-                        epoch,
-                        batch_idx * len(spectrograms),
-                        data_len,
-                        100.0 * batch_idx / len(train_loader),
-                        loss.item(),
-                    )
-                )
+def _levenstein_distance(ref, hyp):
+    pass
 
 
-def test(model, device, test_loader, criterion, epoch, iter_meter, experiment):
-    print("\n Evaluating")
-    model.eval()
-    test_loss = 0
-    test_cer, test_wer = [], []
-    with experiment.test():
-        with torch.no_grad():
-            for I, _data in enumerate(test_loader):
-                spectrograms, labels, input_lengths, label_lengths = _data
-                spectrograms, labels = spectrograms.to(device), labels.to(device)
-                output = model(spectrograms)  # (batch, time, n_class)
-                output = F.log_softmax(output, dim=2)
-                output = output.transpose(0, 1)  # (time, batch, n_class)
-
-                loss = criterion(output, labels, input_lengths, label_lengths)
-                test_loss += loss.item() / len(test_loader)
-
-                decoded_preds, decoded_targets = GreedyDecoder(
-                    output.transpose(0, 1), labels, label_lengths
-                )
-                for j in range(len(decoded_preds)):
-                    test_cer.append(cer(decoded_targets[j], decoded_preds[j]))
-                    test_wer.append(wer(decoded_targets[j], decoded_preds[j]))
-        avg_cer = sum(test_cer) / len(test_cer)
-        avg_wer = sum(test_wer) / len(test_wer)
-
-        experiment.log_metric("test_loss", test_loss, step=iter_meter.get())
-        experiment.log_metric("cer", avg_cer, step=iter_meter.get())
-        experiment.log_metric("wer", avg_wer, step=iter_meter.get())
-
-        print(
-            "Test set: Average loss: {:.4f}, Average CER: {:.4f} Average WER: {:.4f}\n".format(
-                test_loss, avg_cer, avg_wer
-            )
-        )
