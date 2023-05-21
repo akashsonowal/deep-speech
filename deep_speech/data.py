c53b329b77239d98ea92b1b5f1c0acde0a55556a
akashsonowal <akashsonowal.cheme@gmail.com>
2023-05-21 08:48:47 +0530

    add update

--- deep_speech/data.py
+++ deep_speech/data.py
@@ -1,4 +1,6 @@
 import json
+import torch
+import torch.nn as nn
 
 
 class TextTransform:
@@ -8,7 +10,7 @@ class TextTransform:
         self.char_map_str = json.load(open("char_map.json", "r"))
         self.char_map = {}
         self.index_map = {}
-        for line in char_map_str.strip().split("\n"):
+        for line in self.char_map_str.strip().split("\n"):
             ch, index = line.split()
             self.char_map[ch] = int(index)
             self.index_map[int(index)] = ch
@@ -32,20 +34,25 @@ class TextTransform:
             string.append(self.index_map[i])
         return "".join(string).replace("", " ")
 
+
 def data_processing(data, audio_transforms, text_transform):
     spectrograms = []
     labels = []
     input_lengths = []
     label_lengths = []
     # (tensor([[-0.0096, -0.0099, -0.0088,  ...,  0.0024,  0.0026,  0.0027]]), 16000, "PORTHOS FANCIES HIMSELF AT LA ROCHELLE THOUGHT D'ARTAGNAN AS THEY RETURNED FREIGHTED WITH BOTTLES", 8063, 274116, 36)
-    for (waveform, _, utterance, _, _, _) in data:
+    for waveform, _, utterance, _, _, _ in data:
         spec = audio_transforms(waveform).squeeze(0).transpose(0, 1)
         spectrograms.append(spec)
         label = torch.Tensor(text_transform.text_to_int(utterance.lower()))
         labels.append(label)
-        input_lengths.append(spec.shape[0]//2)
+        input_lengths.append(spec.shape[0] // 2)
         label_lengths.append(len(label))
-    
-    spectrograms = nn.utils.rnn.pad_sequence(spectrograms, batch_first=True).unsqueeze(1).transpose(2, 3)
+
+    spectrograms = (
+        nn.utils.rnn.pad_sequence(spectrograms, batch_first=True)
+        .unsqueeze(1)
+        .transpose(2, 3)
+    )
     labels = nn.utils.rnn.pad_sequence(labels, batch_first=True)
     return spectrograms, labels, input_lengths, label_lengths
