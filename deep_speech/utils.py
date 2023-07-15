15180ccbd9fd646a2d880109a92e7d6670eec166
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-15 17:05:56 +0530

    update

--- deep_speech/utils.py
+++ deep_speech/utils.py
@@ -1,8 +1,10 @@
 import torch
 import torch.nn as nn
 
+
 class TextTransform:
     """Maps characters to integers and vice versa"""
+
     def __init__(self):
         self.char_map_str = open("../char_map.txt", "r").read()
         self.char_map = {}
@@ -31,6 +33,7 @@ class TextTransform:
             string.append(self.index_map[i])
         return "".join(string).replace("", " ")
 
+
 def data_processing(data, audio_transforms, text_transform):
     spectrograms = []
     labels = []
@@ -38,32 +41,48 @@ def data_processing(data, audio_transforms, text_transform):
     label_lengths = []
     # (tensor([[-0.0096, -0.0099, -0.0088,  ...,  0.0024,  0.0026,  0.0027]]), 16000, "PORTHOS FANCIES HIMSELF AT LA ROCHELLE THOUGHT D'ARTAGNAN AS THEY RETURNED FREIGHTED WITH BOTTLES", 8063, 274116, 36)
     for waveform, _, utterance, _, _, _ in data:
-        spec = audio_transforms(waveform).squeeze(0).transpose(0, 1) # (time_steps, feature_dim)
+        spec = (
+            audio_transforms(waveform).squeeze(0).transpose(0, 1)
+        )  # (time_steps, feature_dim)
         spectrograms.append(spec)
         label = torch.Tensor(text_transform.text_to_int(utterance.lower()))
         labels.append(label)
-        input_lengths.append(spec.shape[0] // 2) # divided by 2 to downsample
+        input_lengths.append(spec.shape[0] // 2)  # divided by 2 to downsample
         label_lengths.append(len(label))
 
     spectrograms = (
         nn.utils.rnn.pad_sequence(spectrograms, batch_first=True)
-        .unsqueeze(1) #(batch_size, 1, max_time_steps, feature_dim) 
-        .transpose(2, 3) #(batch_size, 1, feature_dim, max_time_steps) 
-    ) 
-    labels = nn.utils.rnn.pad_sequence(labels, batch_first=True) #(batch_size, max_label_length)
+        .unsqueeze(1)  # (batch_size, 1, max_time_steps, feature_dim)
+        .transpose(2, 3)  # (batch_size, 1, feature_dim, max_time_steps)
+    )
+    labels = nn.utils.rnn.pad_sequence(
+        labels, batch_first=True
+    )  # (batch_size, max_label_length)
     return spectrograms, labels, input_lengths, label_lengths
 
-def GreedyDecoder(output, labels, label_lengths, text_transform, blank_label=28, collapse_repeated=True):
+
+def GreedyDecoder(
+    output,
+    labels,
+    label_lengths,
+    text_transform,
+    blank_label=28,
+    collapse_repeated=True,
+):
     arg_maxes = torch.argmax(output, dim=2)
     decodes = []
     targets = []
     for i, args in enumerate(arg_maxes):
         decode = []
-        targets.append(text_transform.int_to_text(labels[i][:label_lengths[i]].tolist()))
+        targets.append(
+            text_transform.int_to_text(labels[i][: label_lengths[i]].tolist())
+        )
         for j, index in enumerate(args):
             if index != blank_label:
-                if collapse_repeated and j!=0 and index == args[j - 1]: # the current index is the same as the previous index
-                    continue 
+                if (
+                    collapse_repeated and j != 0 and index == args[j - 1]
+                ):  # the current index is the same as the previous index
+                    continue
                 decode.append(index.item())
         decodes.append(text_transform.int_to_text(decode))
     return decodes, targets
