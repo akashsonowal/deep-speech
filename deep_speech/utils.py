1fdd3b28f35352b1dfac76d38a39407452594167
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-15 17:44:12 +0530

    update

--- deep_speech/utils.py
+++ deep_speech/utils.py
@@ -77,7 +77,7 @@ def GreedyDecoder(
         targets.append(
             text_transform.int_to_text(labels[i][: label_lengths[i]].tolist())
         )
-        for j, index in enumerate(args): # iterate on a single sequence length
+        for j, index in enumerate(args):  # iterate on a single sequence length
             if index != blank_label:
                 if (
                     collapse_repeated and j != 0 and index == args[j - 1]
@@ -86,6 +86,7 @@ def GreedyDecoder(
                 decode.append(index.item())
         decodes.append(text_transform.int_to_text(decode))
     return decodes, targets
+
 
 class IterMeter(object):
     """Keeps track of total iterations"""
