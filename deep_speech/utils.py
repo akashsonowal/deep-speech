90d2b188e414f541d4d12385b4079a6ca66750a9
Akash Sonowal <work.akashsonowal@gmail.com>
2023-08-09 11:44:53 +0530

    update

--- deep_speech/utils.py
+++ deep_speech/utils.py
@@ -9,7 +9,7 @@ class TextTransform:
         self.char_map_str = open("char_map.txt", "r").read()
         self.char_map = {}
         self.index_map = {}
-        for line in self.char_map_str.strip().split("\n"):
+        for line in self.char_map_str.strip().split("\n"): # remove leading and trailing whitespace with strip()
             ch, index = line.split()
             self.char_map[ch] = int(index)
             self.index_map[int(index)] = ch
